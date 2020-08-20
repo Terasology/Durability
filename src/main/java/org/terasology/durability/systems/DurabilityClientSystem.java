2d67dd4aa77b2048d7fbf34d12c27d362a38b872
DarkWeird <foxflameless@gmail.com>
2020-08-20 15:29:44 +0300

    fix: NUI extraction compatibility

--- src/main/java/org/terasology/durability/systems/DurabilityClientSystem.java
+++ src/main/java/org/terasology/durability/systems/DurabilityClientSystem.java
@@ -1,20 +1,8 @@
-/*
- * Copyright 2014 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.durability.systems;
 
+import org.terasology.math.JomlUtil;
 import org.terasology.utilities.Assets;
 import org.terasology.assets.ResourceUrn;
 import org.terasology.durability.components.DurabilityComponent;
@@ -23,15 +11,14 @@ import org.terasology.entitySystem.event.ReceiveEvent;
 import org.terasology.entitySystem.systems.BaseComponentSystem;
 import org.terasology.entitySystem.systems.RegisterMode;
 import org.terasology.entitySystem.systems.RegisterSystem;
-import org.terasology.math.geom.Rect2i;
-import org.terasology.math.geom.Vector2i;
+import org.joml.Vector2i;
 import org.terasology.rendering.assets.texture.Texture;
 import org.terasology.rendering.assets.texture.TextureUtil;
-import org.terasology.rendering.nui.Canvas;
-import org.terasology.rendering.nui.Color;
+import org.terasology.nui.Canvas;
+import org.terasology.nui.Color;
 import org.terasology.rendering.nui.layers.ingame.inventory.GetItemTooltip;
 import org.terasology.rendering.nui.layers.ingame.inventory.InventoryCellRendered;
-import org.terasology.rendering.nui.widgets.TooltipLine;
+import org.terasology.nui.widgets.TooltipLine;
 
 /**
  * System that handles the UI parts of displaying the durability for the client.
@@ -68,10 +55,10 @@ public class DurabilityClientSystem extends BaseComponentSystem {
 
             ResourceUrn barTexture = TextureUtil.getTextureUriForColor(terasologyColor);
 
-            canvas.drawTexture(Assets.get(backgroundTexture, Texture.class).get(), Rect2i.createFromMinAndMax(minX, minY, maxX, maxY));
+            canvas.drawTexture(Assets.get(backgroundTexture, Texture.class).get(), JomlUtil.rectangleiFromMinAndSize(minX, minY, maxX, maxY));
             int durabilityBarLength = (int) (durabilityPercentage * (maxX - minX - 1));
             int durabilityBarHeight = maxY - minY - 1;
-            canvas.drawTexture(Assets.get(barTexture, Texture.class).get(), Rect2i.createFromMinAndSize(minX + 1, minY + 1, durabilityBarLength, durabilityBarHeight));
+            canvas.drawTexture(Assets.get(barTexture, Texture.class).get(), JomlUtil.rectangleiFromMinAndSize(minX + 1, minY + 1, durabilityBarLength, durabilityBarHeight));
         }
     }
 
